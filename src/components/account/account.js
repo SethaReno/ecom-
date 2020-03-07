7b0c57a03abcf164acb46e44c019ab27c9ea7961
SethaReno <51383197+SethaReno@users.noreply.github.com>
2020-03-07 10:59:50 -0700

    finished navbar feature

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
-
 import * as actions from '../../actions';
 
 import AccountInformation from './accountInformation';
-
 import PurchaseHistory from './purchaseHistory';
 
 class Account extends Component {
@@ -18,35 +16,34 @@ class Account extends Component {
                 title: 'Purchase History',
                 active: false,
                 component: <PurchaseHistory/>
-                
-
             },
             {
+                _id: 1,
                 title: 'Account Information',
                 active: true,
                 component: <AccountInformation/>
-            },
+            }
         ]
 
-            this.props.setHeaderLinks([]);
-            this.props.setNavbarLinks([navbarLinks]);
+        this.props.setHeaderLinks([]);
+        this.props.setNavbarLinks(navbarLinks);
     }
 
     renderContent() {
         let jsx;
-       if(this.props.navbarLinks) {
-           this.props.navbarLinks.forEach(link => {
-              if(link.active) {
-                  jsx= link.component;
-              }
-           })
-       }
-       return jsx;
+        if(this.props.navbarLinks) {
+            this.props.navbarLinks.forEach(link => {
+                if(link.active) {
+                    jsx = link.component;
+                }
+            })
+        }
+        return jsx;
     }
 
     render() {
         return (
-            <div> 
+            <div>
                 { this.renderContent() }
             </div>
         )
